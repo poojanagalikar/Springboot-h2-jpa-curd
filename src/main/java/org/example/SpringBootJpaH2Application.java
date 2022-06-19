295d1573beb0e6fe5a3007c2fd119e84041e333d
pooja nagalikar <poojarnagalikar2710@gmail.com>
2022-06-19 20:09:37 +0530

    first commit

---
+++ src/main/java/org/example/SpringBootJpaH2Application.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaH2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaH2Application.class, args);
+    }
+}
